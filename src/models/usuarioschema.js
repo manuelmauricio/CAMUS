5c4fb25a0700a2c1874ae223478d70cd3fb7e56d
Jonathan Garza <jonyal2010@live.com.mx>
2021-09-13 11:57:04 -0500

    Schemas en proceso

---
+++ src/models/usuarioschema.js
@@ -0,0 +1,61 @@
+const UsuarioSchema = new mongoose.Schema({
+    id_usr: {
+        type: Number,
+        required: true,
+        default: 1
+    },
+    usr: {
+        type: String,
+        required: true,
+        minlength: 4,
+        maxlength: 20,
+        unique: true
+    },
+    pass: {
+        type: String,
+        required: true,
+        minlength: 4,
+        maxlength: 20,
+        unique: true
+    },
+    fecha_nacimiento: {
+        type: Date,
+        required: true
+    },
+    tel1: {
+        type: Number,
+        default: 0
+    },
+    tel2: {
+        type: Number,
+        default: 0
+    },
+    tel1: {
+        type: Number,
+        default: 0
+    },
+    notas: {
+        type: String,
+        minlength: 0,
+        maxlength: 500
+    },
+    email: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 30,
+        unique: true
+    },
+    fecha_alta: {
+        type: Date,
+        required: true
+    },
+    rol: {
+        type: Number,
+        default: 0
+    }
+    
+})
+
+const Usuario = mongoose.model('usuario', UauarioSchema);
+module.exports = Usuario;
