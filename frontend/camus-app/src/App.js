ff544b8a17479c297703189d9051e3ce15ac4ee0
manuelmauricio <55562493+manuelmauricio@users.noreply.github.com>
2021-10-20 17:33:54 -0500

    Página de Login
    Página de Login y mitad de la de Registro

--- frontend/camus-app/src/App.js
+++ frontend/camus-app/src/App.js
@@ -5,7 +5,8 @@ import Conocenos from './pages/Conocenos';
 import Servicios from './pages/Servicios';
 import Directorios from './pages/Directorios';
 import Pacientes from './pages/school/Pacientes';
-
+import Login from './pages/Login';
+import RegistroDeUsuarios from './pages/RegistroDeUsuarios';
 import TopBar from './components/TopBar';
 import { Fragment } from 'react';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
@@ -24,6 +25,9 @@ function App() {
             <Route exact path="/Directorio" component={Directorios} />
 
             <Route exact path="/Pacientes" component={Pacientes} />
+
+            <Route exact path="/Login" component={Login} />
+            <Route exact path="/RegistroDeUsuarios" component={RegistroDeUsuarios} />
           </Switch>
         </NavBar>
       </Router>
---
+++ frontend/camus-app/src/pages/RegistroDeUsuarios.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { TextField } from '@mui/material';
+import { FormControl } from '@mui/material';
+import { Container } from '@mui/material';
+import { styled, alpha } from '@mui/material/styles';
+import Link from '@mui/material/Link';
+
+
+const ColorButton = styled(Button)(({ theme }) => ({
+    color: "#FFFFFF",
+    backgroundColor: "#4d9296",
+    '&:hover': {
+      backgroundColor: "#23585b",
+    },
+  }));
+
+export default function RegistroDeUsuarios() {
+    return (
+        <Card sx={{ minWidth: 275, ml:30, mr:30, mt:5}}>
+        <CardContent alignItems="center">
+        <Box display="flex" justifyContent="center" alignItems="center">
+          <Typography sx={{ fontSize: 40 }} color="text.primary" gutterBottom>
+            Registro de Usuario
+          </Typography>
+          </Box>
+  
+          <Container>
+          <form>
+              <FormControl fullWidth sx={{m:1}}>
+              <TextField name="username" label="Nombre de Usuario" variant="outlined" required></TextField>
+              </FormControl>
+  
+              <FormControl fullWidth sx={{m:1}}>
+              <TextField name="password" label="Contraseña:" variant="outlined" ></TextField>
+              </FormControl>
+  
+              <FormControl fullWidth sx={{mt:3}}>
+                     
+              </FormControl>
+  
+              <Box display="flex" justifyContent="center" alignItems="center">
+              <FormControl sx={{m:1}} >
+              <ColorButton  type="submit" variant="contained">Registrarse</ColorButton>
+              </FormControl>
+              </Box>
+  
+          </form>
+          </Container>
+  
+        </CardContent>
+      </Card>
+    )
+}
